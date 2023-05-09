e150a1e776432d45a1c01fb813005e2f8634120a
ABay23 <alejandrobay23@gmail.com>
2023-05-08 23:47:24 -0400

    Defining outline

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,7 +1,13 @@
 const Hero = () => {
   return (
     <div className=' flex items-center justify-center flex-col text-center'>
-      Hero
+      <h1>Hi, my name is</h1>
+      <h2>Alejandro Bay</h2>
+      <h3>{`I'm a Software Engineer | Full Stack Web Developer`}</h3>
+      <p>
+        Focusing on building great experiences on web applications, focusing on
+        B2B and B2C
+      </p>
     </div>
   )
 }
