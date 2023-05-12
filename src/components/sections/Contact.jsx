970fa9e2dbec7eaa092f224af6d1f1e4ee12afa8
ABay23 <alejandrobay23@gmail.com>
2023-05-12 13:58:58 -0400

    Added

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,5 +1,30 @@
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div className=' flex flex-col'>
+      <div className='grid grid-cols-2 place-self-center py-24'>
+        <div>
+          <h1 className=' items-start font-roboto text-sm md:text-base text-blue-400'>
+            About Me:
+          </h1>
+          <h2></h2>
+          <p className=' text-start md:text-lg font-roboto text-gray-200'>
+            A self-thought software developer with a strong foundation in
+            computer science and a background in business analysis and
+            operations management. Expertise in developing web applications
+            using the MERN (MongoDB, Express, React, Node.js) stack, and a
+            passion for creating robust and scalable applications that meet the
+            unique needs of each client.
+          </p>
+          <br />
+          <p className=' text-start md:text-lg font-roboto text-gray-200'>
+            {`I'm also starting my journey as a game developer on my free time. `}
+            Looking for a career change to pursue my passion for building
+            applications and working with high-performance teams.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,5 +1,34 @@
 const Projects = () => {
-  return <div>Projects</div>
+  return (
+    <div className=' flex flex-col'>
+      <div className='grid grid-cols-2 place-self-center py-24 md:w-auto lg:w-[1000px]'>
+        <div className=' block lg:w-[500px] md:w-auto'>
+          <h1 className=' items-start font-roboto text-sm md:text-base text-blue-400'>
+            Projects:
+          </h1>
+
+          <h2> {`Inventory Management System "Papelon":`}</h2>
+          <p className=' text-start md:text-lg font-roboto text-gray-200'>
+            A self-thought software developer with a strong foundation in
+            computer science and a background in business analysis and
+            operations management. Expertise in developing web applications
+            using the MERN (MongoDB, Express, React, Node.js) stack, and a
+            passion for creating robust and scalable applications that meet the
+            unique needs of each client.
+          </p>
+          <br />
+          <p className=' text-start md:text-lg font-roboto text-gray-200'>
+            {`I'm also starting my journey as a game developer on my free time. `}
+            Looking for a career change to pursue my passion for building
+            applications and working with high-performance teams.
+          </p>
+        </div>
+        <div className=' block lg:w-[500px] md:w-auto'>
+          <img src='src/assets/Papelon-img-1.png' alt='project1' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Projects
